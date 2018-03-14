623e7ab0f96e9cada44d01827627d958ed36decc
Sammy B <arbitrarynoun@gmail.com>
2018-03-13 22:15:42 -0400

    I broke a bunch of things down, with comments. I really thought this would work, nbut I'm not sure where it's failing. I know there's a null issue, so something's not pulling data.

--- 21 project-pixel-art-maker/designs.js
+++ 21 project-pixel-art-maker/designs.js
@@ -10,15 +10,66 @@ var	height = $('#inputHeight').val();
 var	width = $('#inputWidth').val();
 
 
-// When size is submitted by the user, call makeGrid()
+//SECTION When size is submitted by the user, call makeGrid()
 
-submitButton.click( function () {
-	makeGrid();	
-});
+// I expect this to find the table that was already created in the html
+var table = document.getElementById(pixelCanvas);
+
+// Declaring common language; not sure I ever use it
+var tr = document.createElement("tr");
+var td = document.createElement("td");
+
+// Make a number of tr rows equal to the input value "height" from jQuery
+function newRow(height) {
+	for (var i = 0; i < height; i++) {
+		document.createElement("tr");
+		}
+}
+
+// Make a number of td calls equal to the input value "height" from jQuery
+function makeCell(width) {
+	for (var j = 0; j < width; j++) {
+		document.createElement("td");
+	}
+}
+
+// make a grid by appending a number of rows with a number of cells per row established by the two earlier methods
+function makeGrid() {
+	table.append('newRow.append(makeCell)');
+}
+
+// trigger the makeGrid function when the submit button is clicked, which should be grabbing the values
+submitButton.onClick(table.append(MakeGrid));
+
+
+
+
+
 
 
 
 // Old Stuff I Ditched, Newest to Oldest
+
+
+ /* 
+ / var table = document.getElementById(pixelCanvas);
+ / 
+ / var tr = document.createElement("tr");
+ / var td = document.createElement("td");
+ / 
+ / function makeGrid() {
+ / 	for (var i = 0; i < height; i++) {
+ / 		document.createElement("tr");
+ / 		for (var j = 0; j < width; j++)
+ / 			document.createElement("td");
+ / 	}
+ / }
+ / 
+ / submitButton.onClick(table.append(MakeGrid));  // ERROR: 
+Uncaught TypeError: Cannot read property 'append' of null
+*/
+
+
 
  /*
  / // this thing grabs the values of the inputs and
